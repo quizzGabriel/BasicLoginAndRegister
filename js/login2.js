9d80e4a7f5c4f2cd83fc1b52fe8afb83ce870629
Jesus <jesus.lopezJPZ@outlook.com>
2024-02-28 21:40:30 -0600

    se agrego login2 en js

---
+++ js/login2.js
@@ -0,0 +1,45 @@
+const inputs=document.querySelectorAll('input')
+
+
+
+const expresiones ={
+    email: /^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}$/,
+    password: /^.{8,10}$/
+}
+
+const campos = {
+    email: false,
+    password: false
+}
+
+const validarFormularioLogin = (e) => {
+    switch (e.target.name) {
+        case "email":
+            validarCampo(expresiones.email, e.target,"email");
+            break;
+
+        case "password":
+            validarCampo(expresiones.password, e.target, "password");
+            break;
+    }
+}
+
+function validarCampo(expresion,input,campo){
+    if(expresion.test(input.value)){
+        document.getElementById(`${campo}`).classList.remove('is-invalid');
+        document.getElementById(`${campo}`).classList.add('is-valid');
+        campos[campo]=true;
+    }
+    else{
+        document.getElementById(`${campo}`).classList.add('is-invalid');
+        document.getElementById(`${campo}`).classList.remove('is-valid');
+        campos[campo]=false;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup',validarFormularioLogin);
+    input.addEventListener('blur',validarFormularioLogin);
+
+})
+
