0574e82f6a887d608d21d18370ebc9af9b7a6b23
AndresAB <andres.are.96@gmail.com>
2024-02-29 22:24:11 -0600

    previus

---
+++ js/movimiento.js
@@ -0,0 +1,34 @@
+const pwd = document.getElementById("pwd");
+const chk = document.getElementById("chk");
+// document.getElementById("eye").addEventListener("click", function(){
+//     // pwd.setAttribute("type", "password");
+//     alert("¡Haz hecho clic en la imagen!");
+// });
+
+const validacion = ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$;
+
+pwd.addEventListener("keyup", function(e){
+    
+})
+
+// chk.onchange = function(e){
+//     return pwd.type = chk.checked ? "text" : "password";
+// };
+// chk.addEventListener("change", function(e){
+//     if(chk.checked){
+//         pwd.setAttribute("type", "text");
+//     }
+//     else{
+//         pwd.setAttribute("type", "password");
+//     }
+// })
+
+
+// chk.addEventListener("change", function(e){
+//     if(chk.checked){
+//         pwd.type = "text";
+//     }
+//     else{
+//         pwd.type = "password";
+//     }
+// })
